a5d7541c2be0e66da4762321ef87ec68275c6688
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-12-21 12:57:43 +1100

    Create tour and tours components.

--- vit-react-projects/tour/src/App.jsx
+++ vit-react-projects/tour/src/App.jsx
@@ -1,6 +1,41 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useState, useEffect } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const data = await fetch(url);
+      const tours = await data.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 };
 export default App;
