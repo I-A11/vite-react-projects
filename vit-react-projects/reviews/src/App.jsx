75f0d6aff090b43108b45d8c61af3d296033e13c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-12-30 11:54:30 +1100

    Create prev and next persons.

--- vit-react-projects/reviews/src/App.jsx
+++ vit-react-projects/reviews/src/App.jsx
@@ -1,4 +1,60 @@
+import { useState } from "react";
+import people from "./data";
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronCircleLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronCircleRight />
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
