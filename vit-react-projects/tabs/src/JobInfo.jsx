c5b50d5228c0329e0a54077f290ddeb682c4c7e2
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-01-29 18:21:41 +1100

    Create Duties component and use uuid package as key.

--- vit-react-projects/tabs/src/JobInfo.jsx
+++ vit-react-projects/tabs/src/JobInfo.jsx
@@ -1,8 +1,13 @@
+import Duties from "./Duties";
+
 const JobInfo = ({ jobs }) => {
-  const { company, date, duties, title } = jobs[0];
+  const { company, dates, duties, title } = jobs[0];
   return (
     <article className="job-info">
       <h3>{title}</h3>
+      <span className="job-company">{company}</span>
+      <p className="jon-date">{dates}</p>
+      <Duties duties={duties} />
     </article>
   );
 };
