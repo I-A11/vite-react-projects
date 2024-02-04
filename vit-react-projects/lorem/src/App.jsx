50ea13538f21c456e3da4764cab2cacef7872a67
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-02-04 11:35:47 +1100

    Use Usestate to get the amount of paragraphs will be used.

--- vit-react-projects/lorem/src/App.jsx
+++ vit-react-projects/lorem/src/App.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input type="number" name="amount" id="number" />
+      </form>
+    </section>
+  );
 };
 export default App;
