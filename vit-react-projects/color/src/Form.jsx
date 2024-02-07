643a897ab70ac80b780cc490cd30a75dc430f02e
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-02-07 18:46:40 +1100

    Add color value in html.

--- vit-react-projects/color/src/Form.jsx
+++ vit-react-projects/color/src/Form.jsx
@@ -5,14 +5,23 @@ const Form = () => {
   const handleSubmit = (e) => {};
 
   return (
-    <section>
+    <section className="container">
       <h4>color generator</h4>
-      <form>
+      <form className="color-form" onSubmit={handleSubmit}>
         <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          placeholder="#f15025"
           type="text"
           value={color}
           onChange={(e) => setColor(e.target.value)}
         />
+        <button className="btn" type="submit" style={{ background: color }}>
+          Submit
+        </button>
       </form>
     </section>
   );
