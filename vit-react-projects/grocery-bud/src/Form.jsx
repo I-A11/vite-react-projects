f32160af9421bab827c2a0c549be406b25cf0545
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-02-09 11:01:53 +1100

    Create form component to add items to the app.

---
+++ vit-react-projects/grocery-bud/src/Form.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [newItemName, setNewItemName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newItemName);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          add item
+        </button>
+      </div>
+    </form>
+  );
+};
+export default Form;
