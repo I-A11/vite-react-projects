2cb9c3679721cf6f4900687f73faac5691d4376b
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-02-23 18:45:37 +1100

    Use useRef to get navbar height.

--- vit-react-projects/navbar/src/Navbar.jsx
+++ vit-react-projects/navbar/src/Navbar.jsx
@@ -1,36 +1,50 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
-          <button
-            className="nav-toggle"
-            onClick={() => setShowLinks(!showLinks)}
-          >
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((item) => {
-                const { id, url, text } = item;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyle}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((item) => {
+              const { id, url, text } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
