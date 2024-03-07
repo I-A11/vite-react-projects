b31b8a4eb6cefb4ed4af43fcd60558e00d08ba18
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-03-08 09:29:37 +1100

    Create navbar.

--- vit-react-projects/strapi/src/App.jsx
+++ vit-react-projects/strapi/src/App.jsx
@@ -1,11 +1,9 @@
-import { useGlobalContext } from "./Context";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Submenu from "./Submenu";
 
 const App = () => {
-  const { isSidebarOpen } = useGlobalContext();
   return (
     <main>
       <Navbar />
