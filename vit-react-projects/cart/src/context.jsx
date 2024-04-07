19c7f0590774669a0e06ef74b9ed6945a8afaa18
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-04-07 11:12:42 +1000

    Create reducer function .

--- vit-react-projects/cart/src/context.jsx
+++ vit-react-projects/cart/src/context.jsx
@@ -1,11 +1,17 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
-
+import reducer from "./reducer";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const initialState = {
+    loading: false,
+    cart: [],
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
