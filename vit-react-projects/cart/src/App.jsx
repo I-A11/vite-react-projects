8727213baa262528e2f5223de064443925ade630
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-04-12 11:14:26 +1000

    Use map methods to create a new array with id and object.

--- vit-react-projects/cart/src/App.jsx
+++ vit-react-projects/cart/src/App.jsx
@@ -1,8 +1,18 @@
 // components
-import Navbar from './Navbar';
-import CartContainer from './CartContainer';
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
 
 function App() {
+  const items = [
+    {
+      id: 1,
+      name: "first",
+      price: 10,
+    },
+  ];
+
+  const cartItems = items.map((item) => [item.id, item]);
+  console.log(cartItems);
   return (
     <main>
       <Navbar />
