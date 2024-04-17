4ae776c4bf3313d37869c27d3df579cc594b0627
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-04-17 10:58:00 +1000

    Use map method to store cart items.

--- vit-react-projects/cart/src/context.jsx
+++ vit-react-projects/cart/src/context.jsx
@@ -1,5 +1,6 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 import reducer from "./reducer";
+import cartItems from "./data";
 import {
   CLEAR_CART,
   REMOVE,
@@ -13,7 +14,7 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const initialState = {
     loading: false,
-    cart: [],
+    cart: new Map(cartItems.map((item) => [item.id, item])),
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
